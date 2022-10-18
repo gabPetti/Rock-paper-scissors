17c2f554919b55c54ba56b875a82e7a3cd9a6e8d
gabPetti <gabpettidev@gmail.com>
2022-10-17 22:00:56 -0300

    style and add victories counter function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // create computer choice of either fire, water or plant
+
 function getComputerChoice() {
     var computerChoice = Math.ceil(Math.random() * 3);
 
@@ -13,28 +14,33 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-function playRound() {
-    var playerSelection = prompt("What is your choice: fire, water or plant?").toLowerCase();
+// play a single round of the game 
+
+function playRound(playerSelection, victoriesCounter) {
     var computerSelection = getComputerChoice();
     var selections = playerSelection + " x " + computerSelection;
 
     if (playerSelection == computerSelection) {
         console.log("It's a draw, let's try again");
-        return 0
     } else if (selections == "fire x water" || selections == "water x plant" || selections == "plant x fire") {
         console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-        return -1
     } else {
         console.log(`You won! ${playerSelection} beats ${computerSelection}`);
-        return 1
+        victoriesCounter++;
+        document.querySelector(".results span").innerHTML = victoriesCounter;
     }
 }
 
 function game() {
+    document.getElementById("start-button").style.display = "none";
+    document.querySelector(".content-wrapper").style.display = "flex";
+    document.querySelector(".results span").innerHTML = "0";
+    /*
     var playerVictories = 0;
     var computerVictories = 0;
 
-    for (let i = 1; i <= 5; i++) {
+    var i = 1
+    while (i < 6) {
         var roundResult = playRound();
         while (roundResult == 0) {
             roundResult = playRound();
@@ -51,7 +57,25 @@ function game() {
             console.log("Game Over")
         }
     }
+
+    document.querySelector(".content-wrapper").style.display = "none";
+    document.getElementById("start-button").style.display = "block";
+    */
 }
+
+function showResult(element) {
+    victoriesCounter = Number(document.querySelector(".results span").innerHTML);
+    if (element == "fire") {
+        playRound("fire", victoriesCounter)
+    } else if (element == "water") {
+        playRound("water", victoriesCounter)
+    } else {
+        playRound("plant", victoriesCounter)
+    }
+}
+
+// Theme customizations
+
 var themeButton = document.getElementById("theme-button");
 function themeChange() {
     if(themeButton.classList == "fire") {
@@ -61,13 +85,13 @@ function themeChange() {
         document.querySelectorAll(".fire").classList.remove("fire");
     } else if(themeButton.classList == "water") {
         themeButton.classList.remove("water");
-        themeButton.classList.add("leaf");
-        document.querySelectorAll(".water").classList.add("leaf");
+        themeButton.classList.add("plant");
+        document.querySelectorAll(".water").classList.add("plant");
         document.querySelectorAll(".water").classList.remove("water");
     } else {
-        themeButton.classList.remove("leaf");
+        themeButton.classList.remove("plant");
         themeButton.classList.add("fire");
-        document.querySelectorAll(".leaf").classList.add("fire");
-        document.querySelectorAll(".leaf").classList.remove("leaf");
+        document.querySelectorAll(".plant").classList.add("fire");
+        document.querySelectorAll(".plant").classList.remove("plant");
     }
 }
